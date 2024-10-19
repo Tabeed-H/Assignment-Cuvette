afba7e9978eaf181d8ac92347a01038f379e34c9
tabeed-h <tabeedhameed121@gmail.com>
2024-10-19 12:38:51 +0530

    Enabled cors

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,42 +1,43 @@
-const path = require("path");
+// const path = require("path");
 const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const db = require("./db/db");
+
 const app = express();
 const port = process.env.PORT || 3000;
 
-/**************NECESSARY INCLUDES*********** */
+// Use body-parser middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
-app.options("*", cors());
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,PATCH");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, X-Callback-Type, Content-Type, Accept"
-  );
-  res.header("Cache-Control", "no-cache");
-  if ("OPTIONS" == req.method) {
-    res.send(200);
-  } else {
-    next();
-  }
-});
-//**********IMPORTING ROUTES********* */
+
+// Enable CORS for all routes
+app.use(
+  cors({
+    origin: "*", // You can change this to specific origins in production
+    methods: "GET,PUT,POST,DELETE,PATCH",
+    allowedHeaders: [
+      "Origin",
+      "X-Requested-With",
+      "X-Callback-Type",
+      "Content-Type",
+      "Accept",
+    ],
+  })
+);
+
+// Importing routes
 const errorController = require("../src/middleware/errorHandler");
 const userRoutes = require("../src/modules/user/user.routes");
 const jobRoutes = require("../src/modules/job/job.routes");
-//*****************USING THE ROUTES************************* */
+
+// Using the routes
 app.use("/v1/user", userRoutes);
 app.use("/v1/jobs", jobRoutes);
 app.use(errorController);
 
-//**************************SERVER STARTS***********************************/
-
+// Server starts
 app.listen(port, () => {
   console.log(
     "***************************************************************"
@@ -44,5 +45,5 @@ app.listen(port, () => {
   console.log(
     `Server is up on port ${port}! Started at ${new Date().toUTCString()}`
   );
-  console.log(`*************************************************************`);
+  console.log("*************************************************************");
 });
